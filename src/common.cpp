b5854ec97f367eed2170a8e241e81ffac783247c
William Boyd <boydwr@gmail.com>
2019-11-30 00:46:52 -0700

    simple implementation common files. added Operation class.

--- src/common.cpp
+++ src/common.cpp
@@ -3,16 +3,42 @@
 using namespace std;
 using std::string;
 
-Common::Common() {}
+Operation::Operation():type(0), inputs {}, output(0), cycles(0) {}
+
+Operation::Operation(const Operation& to_copy)
+{
+    type = to_copy.getType();
+    inputs = to_copy.getInputs();
+    output = to_copy.getOutput();
+    cycles = to_copy.getCycles();
+}
+
+void Operation::operator = (const Operation& to_copy)
+{
+    type = to_copy.getType();
+    inputs = to_copy.getInputs();
+    output = to_copy.getOutput();
+    cycles = to_copy.getCycles();
+}
+
+void Operation::add_input(int to_add)
+{
+    inputs.push_back(to_add);
+}
+
+void Operation::list_inputs() const
+{
+    for(auto i: inputs)
+    {
+        cout << i << endl;
+    }
+}
+
+Common::Common():CDFG{}, vertices{} {}
 
 Common::Common(const Common& to_copy)
 {
     CDFG = to_copy.getCDFG();
-}
-
-vector<vector<Operation>> Common::getCDFG() const
-{
-    return CDFG;
 }
 
 void Common::operator = (const Common& to_copy)
@@ -23,4 +49,15 @@ void Common::operator = (const Common& to_copy)
 void Common::add_edge(Operation u, Operation v)
 {
     
+}
+
+void Common::displayCDFG() const
+{
+    for(auto i: CDFG)
+    {
+        for (auto j: i)
+        {
+            cout << j.getType() << endl;
+        }
+    }
 }
--- src/common.h
+++ src/common.h
@@ -2,15 +2,53 @@
 #include <string>
 #include <list>
 #include <vector>
+#include <algorithm>
+#include <iterator>
+
+
+using std::vector;
 
 
 class Operation
 {
     public:
+    Operation();
+    Operation(const Operation& to_copy);
+    void operator = (const Operation& to_copy);
+    ~Operation();
+    inline int getType() const{
+        return type;
+    }
+    inline vector<int> getInputs() const{
+        return inputs;
+    }
+    inline int getOutput() const{
+        return output;
+    }
+    inline int getCycles() const{
+        return cycles;
+    }
+    inline void setType(int to_set){
+        type = to_set;
+    }
+    inline void setInputs(vector<int> to_set){
+        inputs = to_set;
+    }
+    inline void setOutput(int to_set){
+        output = to_set;
+    }
+    inline void setCycles(int to_set){
+        cycles = to_set;
+    }
+    void add_input(int to_add);
+    void list_inputs() const;
+
+    protected:
     int type;
     vector<int> inputs;
     int output;
     int cycles;
+
 };
 
 class Common
@@ -21,14 +59,27 @@ class Common
     void operator = (const Common& to_copy);
     ~Common();
     void add_edge(Operation u, Operation v);
-    void displayCDFG(int size);
-    vector<vector<Operation>>getCDFG() const;
+    void displayCDFG() const;
+    inline vector<vector<Operation>>getCDFG() const{
+        return CDFG;
+    }
+    inline vector<int> getVertices() const{
+        return vertices;
+    }
+    inline void setVertices(vector<int> to_set){
+        vertices = to_set;
+    }
+    inline void setCDFG(vector<vector<Operation>> to_set){
+        CDFG = to_set;
+    }
     
     
     
     protected:
 
     vector<vector<Operation>> CDFG;
+    vector<int> vertices;
+
 
 
 
